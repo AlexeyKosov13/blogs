36e51afa1ea7fcdf87cf91afac09ddeb4612eb82
AlexHome <foster13@yandex.ru>
2022-06-19 14:57:19 +0300

    first

--- src/shared/projectData.js
+++ src/shared/projectData.js
@@ -8,7 +8,7 @@ export const POSTS = [
     {
       id: 2,
       title: "Post 2",
-      description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veritatis,vitae officia eum quis sed aut provident esse! Sunt, aperiam.Tempore corporis mollitia placeat delectus repudiandae animi, itaqueiusto similique aliquid. Possimus officiis neque, doloremque nonesse, ea natus autem modi explicabo quae nemo? Vitae, ex! Modi dictaenim nam quam, consectetur rem placeat vero saepe mollitia, deseruntsint dolor quod.",
+      description: "Lorem ipsum dolor sit amet elit. Veritatis,vitae officia eum quis sed aut provident esse! Sunt, aperiam.Tempore corporis mollitia placeat delectus repudiandae animi, itaqueiusto similique aliquid. Possimus officiis neque, doloremque nonesse, ea natus autem modi explicabo quae nemo? Vitae, ex! Modi dictaenim nam quam, consectetur rem placeat vero saepe mollitia, deseruntsint dolor quod.",
       liked:true,
     },
     {
